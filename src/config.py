2d47e1d1e0be6d35ebc8115e93f9ca6ddebad9cd
charmaine villalobos <charmainechesca.villalobos.cics@ust.edu.ph>
2025-09-01 02:55:38 +0800

    refactor: massive changes

--- src/config.py
+++ src/config.py
@@ -18,11 +18,10 @@ NUMBER_OF_PATIENTS = config["dataset"]["number_of_patients"]
 
 # Preprocessing parameters
 SAMPLE_RATE = config["preprocessing"]["sample_rate"]
-STFT_WINDOW_SIZE = int(SAMPLE_RATE * 30)
-WINDOW_TIME_STEP = STFT_WINDOW_SIZE // 2
 PREICTAL_MINUTES = config["preprocessing"]["preictal_minutes"]
 EPOCH_WINDOW_DURATION_SECONDS = config["preprocessing"]["epoch_window_duration_seconds"]
 LOW_CUTOFF_FILTER = config["preprocessing"]["low_cutoff_filter"]
 HIGH_CUTOFF_FILTER = config["preprocessing"]["high_cutoff_filter"]
 NOTCH_FILTER = config["preprocessing"]["notch_filter"]
 SELECTED_CHANNELS = config["preprocessing"]["selected_channels"]
+BAND_DEFS = config["preprocessing"]["band_defs"]
