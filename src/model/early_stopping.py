5ce1b1dcf3fcadb1003d843713360857cd9e60ea
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-09-27 14:53:44 +0800

    refactor(early_stopping): revamped completely

--- src/model/early_stopping.py
+++ src/model/early_stopping.py
@@ -1,55 +1,38 @@
-import os
-
-import torch
-
 from src.logger import setup_logger
 
 logger = setup_logger(name="early_stopping")
 
 
 class EarlyStopping:
-    def __init__(self, path, patience=7, min_delta=0, verbose=False):
-        """
-        Args:
-            path (str): Full path (including filename) to save the checkpoint.
-            patience (int): Early stopping patience.
-            min_delta (float): Minimum improvement to reset patience.
-            verbose (bool): Verbosity.
-        """
-        # ensure directory exists
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-
-        self.path = path
+    def __init__(self, patience=5, delta=0):
         self.patience = patience
-        self.min_delta = min_delta
-        self.verbose = verbose
-
-        self.counter = 0
+        self.delta = delta
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = float("inf")
+        self.counter = 0
+        self.best_model_state = None
 
     def __call__(self, val_loss, model):
         score = -val_loss
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
-        elif score < self.best_score + self.min_delta:
+            self.best_model_state = model.state_dict()
+
+        elif score < self.best_score + self.delta:
             self.counter += 1
-            if self.verbose:
-                print(f"EarlyStopping counter: {self.counter} out of {self.patience}")
+            logger.info(
+                f"No improvement. EarlyStopping counter: {self.counter} out of {self.patience}"
+            )
+
             if self.counter >= self.patience:
                 self.early_stop = True
+                logger.info(
+                    f"Early stopping triggered after {self.patience} epochs without improvement"
+                )
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.best_model_state = model.state_dict()
+            if self.counter > 0:
+                logger.info("Performance improved - resetting patience counter")
             self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        if self.verbose:
-            logger.info(
-                f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}). Saving model..."
-            )
-        torch.save(model.state_dict(), self.path)
-        self.val_loss_min = val_loss
