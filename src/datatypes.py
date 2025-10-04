4c574820754f9ce1c102e27e3c5babfee548cbe3
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-10-05 02:26:00 +0800

    Revert "refactor(preprocessing): completely revamped the way of loading edf files"
    This reverts commit f75e7e3d45e906b778dc9f5d3dc5e7110dfc62c8.

--- src/datatypes.py
+++ src/datatypes.py
@@ -15,7 +15,6 @@ class EpochInterval:
     end: int
     duration: Optional[int] = None
     windows_created: Optional[int] = None
-    file_name: Optional[str] = None
 
 
 @dataclass
@@ -46,9 +45,3 @@ class PrecomputedStftSummary:
     number_of_seizures: int
     preictal_intervals: int
     interictal_intervals: int
-
-
-@dataclass
-class IntervalFileInfo:
-    file_name: str
-    interval: EpochInterval
